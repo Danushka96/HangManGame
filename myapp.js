0df811cee055d931857abafcc1b603d8915945bb
Hasee Amarathunga <haseeamarathunga@gmail.com>
2018-06-06 13:15:01 -0700

    Added myapp.js

---
+++ myapp.js
@@ -0,0 +1,16 @@
+var myapp=angular.module("MyApp",[]);
+myapp.controller("DemoCTRL",['$scope','$timeout',function($scope,$timeout){
+
+var username=["hasee","banuka","pasan","srinath","isuru","oshada","gayan","pubudu","thusitha"];
+var password=["clash","banuka","pasan","srinath","isuru","oshada","gayan","pubudu","thusitha"];
+
+$scope.inputUser={
+    letter:''
+}
+$scope.inputPass={
+    letter:''
+}
+
+
+
+}]);
