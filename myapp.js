4aaf2af9f700b73cfdac32ef364ba4dc6647e276
Hasitha Amarathunga <haseeamarathunga@gmail.com>
2018-07-07 07:55:23 +0530

    Update myapp.js

--- myapp.js
+++ myapp.js
@@ -2,7 +2,7 @@ var myapp=angular.module("MyApp",[]);
 myapp.controller("DemoCTRL",['$scope','$timeout',function($scope,$timeout){
 
 var username=["hasitha","banuka","pasan","srinath","isuru","bawwa","gayan","sipun","thusitha"];
-var password=["clasher","jala","navoda","mahagamaya","isu","oshada","tojja","pubudu","thusitha"];
+var password=["haseesart","jalas","navodalak","mahagamaya","isurubanda","oshada","tojja","pubudu","thusitha"];
 
 $scope.inputUser={
     letter:''
